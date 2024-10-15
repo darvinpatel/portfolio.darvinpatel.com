82604bacd9faba3cf92bebcaea70ae49c2d3e42d
Darvin <darwindpatel@gmail.com>
2024-10-15 18:37:41 +1100

    added playwright projects

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I help secure our virtual world to safeguard data, resources and infrastructure from malicious actors.
+          Building Secure, Scalable Solutions with Cloud and Automation Expertise
         </h2>
       </div>
     </div>
